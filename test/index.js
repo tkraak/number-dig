59f64b137a715829b8a19323bab6bb2c6489de00
Tom Kraak <thomas.kraak@teamaol.com>
2016-12-13 12:47:00 -0500

    account for multiple instances of d -- fix #1 :bug:

--- test/index.js
+++ test/index.js
@@ -4,3 +4,7 @@ const numberDig = require('../lib/index')
 test('square numbers between 0 - 10 and filter for 1', (t) => {
   t.is(numberDig(10, 1), 4)
 })
+
+test('account for multiple instances of d in a squared number', (t) => {
+  t.is(numberDig(11, 1), 6)
+})
